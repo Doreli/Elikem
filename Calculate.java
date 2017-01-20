3f1156ca6d89cd73fa626d699a3b323d3a63b869
Dorcas <delidoe95@gmail.com>
2017-01-20 18:35:57 +0000

    first3

--- Calculate.java
+++ Calculate.java
@@ -7,7 +7,12 @@ public class Calculate{
     
     System.out.println(num1 + num2);
     }
-  
+  else if(args[0].equalsIgnoreCase("multiply")){
+    double num1 = Double.parseDouble(args[1]);
+    double num2 = Double.parseDouble(args[2]);
+    
+    System.out.println(num1 * num2);
+    }
   }
 
 }
